82c36765f99b65b9121de3d8c7974d19e77d65ba
X Li <83388107+Xinxuan-Li@users.noreply.github.com>
2023-10-24 13:27:37 +1100

    festivals

--- group-project/src/App.js
+++ group-project/src/App.js
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
 	return (
--- group-project/src/components/Footer.js
+++ group-project/src/components/Footer.js
@@ -1,3 +1,24 @@
 import { Component } from "react";
 
-class Footer extends Component {}
+class Footer extends Component {
+	backToMap() {}
+
+	exploreNext() {}
+
+	render() {
+		return (
+			<footer className="footer">
+				<button
+					onClick={() => this.backToMap()}
+					className="back-to-map-btn"
+				></button>
+				<button
+					onClick={() => this.exploreNext()}
+					className="explore-next-country-btn"
+				></button>
+			</footer>
+		);
+	}
+}
+
+export default Footer;
