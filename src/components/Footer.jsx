53005c70b55c66edd76804d7fc970c4669ebba8c
EricaHu <xihu4221@uni.sydney.edu.au>
2023-11-07 11:59:54 +0800

    Add footer animation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,38 @@
-import React from 'react';
-import "../css/footer.css"
+import React from "react";
+import "../css/footer.css";
 
 function Footer({ onWorldMapClick, onRandomCountryClick, isInCountryPage }) {
-
     return (
         <section className="footer">
             <div className="footer-button" onClick={onWorldMapClick}>
-            <img
+                <img
                     className="map-card-arrow"
                     src="./img/icon/button-arrow1.svg"
                     alt="map card arrow"
                 />
-                <img className="map-card-icon" src="./img/icon/world-map.png" alt="map card icon" />
-                <p className="button-text">{isInCountryPage ? "Back to world map": "Discover via world map"}</p>
+                <img
+                    className="map-card-icon"
+                    src="./img/icon/world-map.png"
+                    alt="map card icon"
+                />
+                <p className="footer-text">
+                    {isInCountryPage
+                        ? "Back to world map"
+                        : "Discover via world map"}
+                </p>
                 <div className="button-decor-highlight"></div>
             </div>
             <div className="footer-button" onClick={onRandomCountryClick}>
-                <img className="map-card-icon" src="./img/icon/random-country.png" alt="map card icon" />
-                <p className="button-text">{isInCountryPage ? "Explore next random country": "Explore a random country"}</p>
+                <p className="footer-text">
+                    {isInCountryPage
+                        ? "Explore next random country"
+                        : "Explore a random country"}
+                </p>
+                <img
+                    className="map-card-icon"
+                    src="./img/icon/random-country.png"
+                    alt="map card icon"
+                />
                 <img
                     className="map-card-arrow"
                     src="./img/icon/button-arrow.svg"
